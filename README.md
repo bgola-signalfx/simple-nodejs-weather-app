ff2d8d26c56f7bc8ed83280de9849c6327bfa33d
Bartek Gola <bgola@signalfx.com>
2020-05-21 15:27:20 +0200

    Update readme, add Catchpoint's RUM script

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-# simple-nodejs-weather-app
-Simple Node.js Command Line Weather Application
+## Overview
 
-* Check out the **[Live Demo](https://simple-nodejs-weather-app-irhhpddsku.now.sh/)**
-* Read the full tutorial on how to build this application at [codeburst.io](https://codeburst.io)
-* Run the web app locally:
-```
-node server.js
-// Now open your browser and visit: localhost:3000
-```
-![gif](https://github.com/bmorelli25/simple-nodejs-weather-app/blob/master/giphy.gif?raw=true 'website gif')
+This app shows how to integrate [SignalFx Microservices APM (µAPM)](https://www.splunk.com/en_us/software/microservices-apm.html) and [Catchpoint](https://www.catchpoint.com/) systems.
+
+ * It generates traces on the backend side using SignalFx auto instrumentation [library](https://github.com/signalfx/signalfx-nodejs-tracing).
+ * Then it pushes SignalFx µAPM trace ID as a part of Real User Metrics (RUM) data to the Catchpoint platform.
+
+The server side auto instrumentation happens in the [server.js](server.js) file.
+
+The client side RUM metrics reporting (including SignalFx µAPM trace ID) is configured in the [index.ejs](views/index.ejs) file. 
+
+## Quick Start
+
+1. To report traces to SignalFx µAPM platform you need to [install SignalFx Smart Agent](https://docs.signalfx.com/en/latest/apm/apm-getting-started/apm-smart-agent.html) on a host that runs this app.
+
+1. This app uses [OpenWeather](https://openweathermap.org/) API to get temperature in a given city. Before running the app you need to [obtain the API key](https://openweathermap.org/appid) and set it as an environment variable called `OWM_KEY`.
+
+1. To start the app using your API key you can use the following command (please note the syntax may be a bit different depending on the operation system and shell used):
+   ```
+   $ OWM_KEY=your-owm-key-123 node server.js
+   ```
+1. Now open your browser and visit http://localhost:3000
+
--- server.js
+++ server.js
@@ -1,34 +1,42 @@
+// This is where signalfx-tracing is initialized.
+// All supported packages required() afterwards are going to be automatically instrumented.
+// Refer to https://github.com/signalfx/signalfx-nodejs-tracing for details.
 
 const tracer = require('signalfx-tracing').init({
-  // Optional Smart Agent or Gateway target.  Default is http://localhost:9080/v1/trace
-  url: 'http://localhost:9081/v1/trace'
+  url: 'http://localhost:9080/v1/trace',
+  service: 'sample-weather-app'
 });
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+const app = express();
 
-const apiKey = '*****************';
+const apiKey = process.env.OWM_KEY;
+
+if (!apiKey) {
+  console.log('OWM_KEY not set. Make sure to set it to a valid https://openweathermap.org/ API key.');
+  process.exit(1)
+}
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null, traceID: req.sfx.traceId});
-})
+  res.render('index', {weather: null, error: null, traceID: req.sfx.traceId}); // <-- this is where trace ID is passed on to the view layer
+});
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
   request(url, function (err, response, body) {
-    if(err){
+    if (err) {
       res.render('index', {weather: null, error: 'Error, please try again', traceID: req.sfx.traceId});
     } else {
       let weather = JSON.parse(body)
-      if(weather.main == undefined){
+      if (weather.main === undefined) {
         res.render('index', {weather: null, error: 'Error, please try again', traceID: req.sfx.traceId});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
@@ -36,8 +44,8 @@ app.post('/', function (req, res) {
       }
     }
   });
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
